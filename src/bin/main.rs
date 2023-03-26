d1f0926233c4f6e30c6fec346207862a21d793fc
FreddyWordingham <freddy@digilab.co.uk>
2023-03-27 00:57:16 +0100

    Improved display method

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use ndarray::Array2;
+use std::{thread::sleep, time};
 
 use constellation::Galaxy;
 
@@ -22,21 +23,31 @@ fn main() {
     let args = Args::parse();
 
     let galaxy = Galaxy::new(args.num_stars, args.radius);
-    let count = galaxy.count(args.res);
 
-    display(&count);
+    loop {
+        let count = galaxy.count(args.res);
+        display(&count);
+        sleep(time::Duration::from_millis(100));
+        break;
+    }
 }
 
 /// Print the galaxy count to the terminal.
 fn display(count: &Array2<u8>) {
-    for row in count.genrows() {
+    let mut buffer = String::new();
+
+    for row in count.rows() {
         for &val in row.iter() {
-            if val == 0 {
-                print!("   ");
-            } else {
-                print!("{0:>3}", val);
+            match val {
+                0 => buffer.push_str("   "),
+                1 => buffer.push_str(" . "),
+                2 => buffer.push_str(" : "),
+                3 => buffer.push_str(" * "),
+                x => buffer.push_str(&format!(" {} ", x)),
             }
         }
-        println!();
+        buffer.push('\n');
     }
+
+    print!("\x1B[2J\x1B[1;1H{}", buffer);
 }
