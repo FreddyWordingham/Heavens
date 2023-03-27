7d20044a83fcd98b9d4334ad4c19f4710af98d1c
FreddyWordingham <freddy@digilab.co.uk>
2023-03-27 22:05:08 +0100

    Improved buffer usage

--- src/nbody.rs
+++ src/nbody.rs
@@ -1,7 +1,7 @@
 //! N-Body calculations.
 
 use nalgebra::Vector3;
-use ocl::ProQue;
+use ocl::{builders::BufferBuilder, ProQue};
 
 use crate::Particle;
 
@@ -14,28 +14,28 @@ pub fn nbody(particles: &mut [Particle], grav_strength: f64, smoothing_length: f
 
     let src = format!(
         "
-        __kernel void nbody(__global float* buffer_x, __global float* buffer_y, __global float* buffer_z, __global float* positions_x, __global float* positions_y, __global float* positions_z) {{
+        __kernel void nbody(__global float* buffer, __global float* positions) {{
             int n = get_global_id(0);
 
-            float x = positions_x[n];
-            float y = positions_y[n];
-            float z = positions_z[n];
+            float x = positions[(n * 3) + 0];
+            float y = positions[(n * 3) + 1];
+            float z = positions[(n * 3) + 2];
 
             for (int m = 0; m < {num_particles}; ++m) {{
                 if (n == m) {{
                     continue;
                 }}
 
-                float dx = positions_x[m] - x;
-                float dy = positions_y[m] - y;
-                float dz = positions_z[m] - z;
+                float dx = positions[(m * 3) + 0] - x;
+                float dy = positions[(m * 3) + 1] - y;
+                float dz = positions[(m * 3) + 2] - z;
 
                 float r = max(sqrt((dx * dx) + (dy * dy) + (dz * dz)), (float){smoothing_length});
                 float r_inv = 1.0 / (r * r * r);
 
-                buffer_x[n] += r_inv * dx * {grav_strength};
-                buffer_y[n] += r_inv * dy * {grav_strength};
-                buffer_z[n] += r_inv * dz * {grav_strength};
+                buffer[(n * 3) + 0] += r_inv * dx * {grav_strength};
+                buffer[(n * 3) + 1] += r_inv * dy * {grav_strength};
+                buffer[(n * 3) + 2] += r_inv * dz * {grav_strength};
             }}
         }}
         ",
@@ -48,46 +48,40 @@ pub fn nbody(particles: &mut [Particle], grav_strength: f64, smoothing_length: f
         .dims(particles.len())
         .build()
         .unwrap();
-    let gpu_buffer_x = pro_que.create_buffer::<f32>().unwrap();
-    let gpu_buffer_y = pro_que.create_buffer::<f32>().unwrap();
-    let gpu_buffer_z = pro_que.create_buffer::<f32>().unwrap();
+    let gpu_buffer = pro_que
+        .buffer_builder::<f32>()
+        .len(particles.len() * 3)
+        .build()
+        .unwrap();
 
-    let positions_x = pro_que.create_buffer::<f32>().unwrap();
-    let positions_y = pro_que.create_buffer::<f32>().unwrap();
-    let positions_z = pro_que.create_buffer::<f32>().unwrap();
-    let mut cpu_buffer_x = vec![0.0f32; gpu_buffer_x.len()];
-    let mut cpu_buffer_y = vec![0.0f32; gpu_buffer_y.len()];
-    let mut cpu_buffer_z = vec![0.0f32; gpu_buffer_z.len()];
+    let positions = pro_que
+        .buffer_builder::<f32>()
+        .len(particles.len() * 3)
+        .build()
+        .unwrap();
+    let mut cpu_buffer = vec![0.0f32; gpu_buffer.len()];
     for (n, particle) in particles.iter().enumerate() {
-        cpu_buffer_x[n] = particle.pos.x as f32;
-        cpu_buffer_y[n] = particle.pos.y as f32;
-        cpu_buffer_z[n] = particle.pos.z as f32;
+        cpu_buffer[(n * 3) + 0] = particle.pos.x as f32;
+        cpu_buffer[(n * 3) + 1] = particle.pos.y as f32;
+        cpu_buffer[(n * 3) + 2] = particle.pos.z as f32;
     }
-    positions_x.write(&cpu_buffer_x).enq().unwrap();
-    positions_y.write(&cpu_buffer_y).enq().unwrap();
-    positions_z.write(&cpu_buffer_z).enq().unwrap();
+    positions.write(&cpu_buffer).enq().unwrap();
 
     let kernel = pro_que
         .kernel_builder("nbody")
-        .arg(&gpu_buffer_x)
-        .arg(&gpu_buffer_y)
-        .arg(&gpu_buffer_z)
-        .arg(&positions_x)
-        .arg(&positions_y)
-        .arg(&positions_z)
+        .arg(&gpu_buffer)
+        .arg(&positions)
         .build()
         .unwrap();
     unsafe { kernel.enq().unwrap() };
 
-    gpu_buffer_x.read(&mut cpu_buffer_x).enq().unwrap();
-    gpu_buffer_y.read(&mut cpu_buffer_y).enq().unwrap();
-    gpu_buffer_z.read(&mut cpu_buffer_z).enq().unwrap();
+    gpu_buffer.read(&mut cpu_buffer).enq().unwrap();
 
     for (n, particle) in particles.iter_mut().enumerate() {
         particle.vel += Vector3::new(
-            cpu_buffer_x[n] as f64,
-            cpu_buffer_y[n] as f64,
-            cpu_buffer_z[n] as f64,
+            cpu_buffer[(n * 3) + 0] as f64,
+            cpu_buffer[(n * 3) + 1] as f64,
+            cpu_buffer[(n * 3) + 2] as f64,
         );
         particle.pos += particle.vel * dt;
     }
