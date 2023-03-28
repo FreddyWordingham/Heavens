2266df53d450b65dd67ea2ff08df27d9d67908ac
FreddyWordingham <freddy@digilab.co.uk>
2023-03-28 23:15:12 +0100

    Improved input typing

--- src/galaxy.rs
+++ src/galaxy.rs
@@ -1,8 +1,11 @@
+//! Galaxy input parameters.
+
 use nalgebra::Vector3;
 use serde::Deserialize;
 
+/// Complete information to construct an initial galaxy.
 #[derive(Debug, Deserialize)]
 pub struct Galaxy {
     /// Centre.
-    pub pos: [f32; 3],
+    pub pos: Vector3<f32>,
 }
