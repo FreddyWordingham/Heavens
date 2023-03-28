13d4a826ed8c2b9884f2ecedae04bdac5a373c4b
FreddyWordingham <freddy@digilab.co.uk>
2023-03-28 22:04:46 +0100

    Added basic arguments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,8 +2,19 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Args {}
+struct Args {
+    #[arg(short, long)]
+    grav_strength: f32,
+
+    #[arg(short, long)]
+    smoothing_length: f32,
+
+    #[clap(short, long, value_parser, num_args = 2.., value_delimiter = ' ')]
+    cmap: Vec<String>,
+}
 
 fn main() {
+    let args = Args::parse();
     println!("Hello, Galaxy!");
+    println!("{:?}", args);
 }
